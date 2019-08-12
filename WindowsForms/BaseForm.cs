6a06f78749a91d230f6f8fc50cbabaa65da9f034
Sopheak088 <ourlesson031@gmail.com>
2019-08-12 08:20:47 +0700

    Add project files.

---
+++ WindowsForms/BaseForm.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsForms
+{
+    public static class BaseForm
+    {
+        public static void SetForm(Form frm, FormBorderStyle formBorderStyle = FormBorderStyle.FixedSingle, FormWindowState formWindowState = FormWindowState.Normal,
+                                    FormStartPosition formStartPosition = FormStartPosition.CenterScreen)
+        {
+            frm.ControlBox = false;
+            frm.FormBorderStyle = formBorderStyle;
+            frm.WindowState = formWindowState;
+            frm.StartPosition = formStartPosition;
+        }
+
+        /// <summary>
+        /// Return true if you click on Yes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        public static bool Yes_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            bool isYes = true;
+            // Display a MsgBox asking the user to save changes or abort.
+            if (MessageBox.Show("Do you want to close application?", "Close",
+               MessageBoxButtons.YesNo) != DialogResult.Yes)
+            {
+                // Cancel the Closing event from closing the form.
+                e.Cancel = true;
+                isYes = false;
+                // Call method to save file...
+            }
+            return isYes;
+        }
+
+        public static DialogResult Ask_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Do you want to close application?", "Close", MessageBoxButtons.YesNoCancel);
+            // Display a MsgBox asking the user to save changes or abort.
+            if (result == DialogResult.Yes)
+            {
+                // Cancel the Closing event from closing the form.
+                //e.Cancel = true;
+                result = DialogResult.Yes;
+                // Call method to save file...
+            }
+            if (result == DialogResult.No)
+            {
+                //e.Cancel = false;
+                result = DialogResult.No;
+            }
+            if (result == DialogResult.Cancel)
+            {
+                result = DialogResult.Cancel;
+            }
+            return result;
+        }
+    }
+}
---
+++ WindowsForms/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // BaseForm.SetForm(this);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //string data = "";
+            //StreamReader reader = new StreamReader(@"C:\WINDOWS\system32");
+            //StreamWriter writer = new StreamWriter(@"C:\WINDOWS\system32");
+            //IPAddress address = IPAddress.Parse("10.198.1.8");
+            //IPEndPoint ipe = new IPEndPoint(address, 23);
+            //Socket telnetSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //telnetSocket.Connect(ipe);
+            //NetworkStream NsStream = new NetworkStream(telnetSocket, true);
+            //if (telnetSocket.Connected)
+            //{
+            //    NsStream = new NetworkStream(telnetSocket, true);
+            //    reader = new StreamReader(NsStream);
+            //}
+
+            //while (!reader.EndOfStream)
+            //{
+            //    data = reader.ReadLine();
+            //    if (data.Contains("Password:"))
+            //    {
+            //        //I want to enter password in cmd here
+            //    }
+            //}
+            //reader.Close();
+            //if (NsStream == null)
+            //    NsStream.Close();
+
+            /*
+            //create a new telnet connection to hostname "gobelijn" on port "23"
+            TelnetConnection tc = new TelnetConnection("10.10.248.30", 23);
+
+            //login with user "root",password "rootpassword", using a timeout of 100ms,
+            //and show server output
+            string s = tc.Login("bidc", "bidc@123", 1000);
+            Console.Write(s);
+
+            //Console.Write("ping 10.22.7.1");
+            // server output should end with "$" or ">", otherwise the connection failed
+            string prompt = s.TrimEnd();
+            prompt = s.Substring(prompt.Length - 1, 1);
+            try
+            {
+                if (prompt != "$" && prompt != ">") prompt = "";
+            }
+            catch (Exception xception)
+            {
+                MessageBox.Show("Connection failed");
+            }
+
+            // throw new Exception("Connection failed");
+
+            // while connected
+            while (tc.IsConnected || prompt.Trim() != "exit")
+            {
+                // display server output
+                Console.Write(tc.Read());
+
+                // send client input to server
+                prompt = Console.ReadLine();
+                tc.WriteLine(prompt);
+
+                // display server output
+                Console.Write(tc.Read());
+            }
+
+            Console.WriteLine("***DISCONNECTED");
+            Console.ReadLine();
+
+            */
+            /*  WebsitesScreenshot.WebsitesScreenshot _Obj;
+              _Obj = new WebsitesScreenshot.WebsitesScreenshot();
+              WebsitesScreenshot.WebsitesScreenshot.Result _Result;
+              System.Drawing.Bitmap _MyBitmap = null;
+              _Result = _Obj.CaptureWebpage("https://www.google.com");
+              if (_Result == WebsitesScreenshot.WebsitesScreenshot.Result.Captured)
+              {
+                  //Get captured image in memory
+                  _MyBitmap = _Obj.GetImage();
+                  _MyBitmap.Save("D:\\test.png");
+                  _Obj.Dispose();
+              }*/
+            /*
+          HtmlCapture capture = new HtmlCapture(@"C:\Users\User\Desktop\Document\WindowsForms\test.jpg");
+          capture.HtmlImageCapture += new HtmlCapture.HtmlCaptureEvent(capture_HtmlImageCapture);
+          capture.Create("https://www.google.com");*/
+            /* Image bit = new Bitmap(Screen.PrimaryScreen.WorkingArea.Width, Screen.PrimaryScreen.WorkingArea.Height);
+
+             Graphics gs = Graphics.FromImage(bit);
+
+             gs.CopyFromScreen(new Point(0, 0), new Point(0, 0), bit.Size);
+
+             //bit.Save(@"D:\Test.jpg"); // or stream to memory, whichever you prefer
+
+             string url = "https://www.bidc.com.kh/";
+             string content = "";
+
+             WebRequest webRequest = WebRequest.Create(url);
+             WebResponse webResponse = webRequest.GetResponse();
+             StreamReader sr = new StreamReader(webResponse.GetResponseStream(), System.Text.Encoding.GetEncoding("UTF-16"));
+             content = sr.ReadToEnd();
+             //save to file
+             //byte[] b = Convert.FromBase64String(content.Replace("-", ""));// GetBytes(content);
+             // Convert.FromBase64String(content);
+             //MemoryStream ms = new MemoryStream(b);
+             //Image img = Image.FromStream(ms);
+             //img.Save(@"D:\pic.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+             //img.Dispose();
+             //ms.Close();
+
+             //using (Image image = Image.FromStream(new MemoryStream(b)))
+             //{
+             //    image.Save(@"D:\pic.jpg", ImageFormat.Jpeg);  // Or Png
+             //    image.Dispose();
+             //}
+
+             //MemoryStream ms = new MemoryStream(b, 0, b.Length);
+
+             //ms.Write(b, 0, b.Length);
+             //Image image = Image.FromStream(ms, true);
+             //image.Save(@"D:\pic.jpg", ImageFormat.Jpeg);*/
+        }
+
+        private byte[] GetBytes(string str)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //if (BaseForm.Ask_FormClosing(sender, e).Equals(DialogResult.Yes))
+            //{
+            //    MessageBox.Show("Yes");
+            //}
+            //else if (BaseForm.Ask_FormClosing(sender, e).Equals(DialogResult.No))
+            //{
+            //    MessageBox.Show("No");
+            //}
+            //else
+            //{
+            //    e.Cancel = true;
+            //}
+
+
+            //if (BaseForm.Yes_FormClosing(sender, e))
+            //{
+            //    MessageBox.Show("Yes");
+            //}
+        }
+
+        private void ContextMenuStrip1_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            textBox1.BorderStyle = BorderStyle.None;
+            Pen p = new Pen(Color.CornflowerBlue);
+            Graphics g = e.Graphics;
+            int variance = 3;
+            g.DrawRectangle(p, new Rectangle(textBox1.Location.X - variance, textBox1.Location.Y - variance, textBox1.Width + variance, textBox1.Height + variance));
+        }
+    }
+}
