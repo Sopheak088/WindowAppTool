ec8c0b82b67987fe284866513a64ec695a5b6332
Sopheak088 <ourlesson031@gmail.com>
2019-09-09 14:44:43 +0700

    find Memory percent

--- WindowsForms/Form1.Designer.cs
+++ WindowsForms/Form1.Designer.cs
@@ -28,37 +28,156 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblRamTotal = new System.Windows.Forms.Label();
+            this.lblRamFree = new System.Windows.Forms.Label();
+            this.lblRamPercent = new System.Windows.Forms.Label();
+            this.lblCpuPercent = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // timer1
             // 
-            this.textBox1.Location = new System.Drawing.Point(238, 31);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(196, 20);
-            this.textBox1.TabIndex = 0;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // lblRamTotal
+            // 
+            this.lblRamTotal.AutoSize = true;
+            this.lblRamTotal.Font = new System.Drawing.Font("Hanuman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRamTotal.Location = new System.Drawing.Point(413, 9);
+            this.lblRamTotal.Name = "lblRamTotal";
+            this.lblRamTotal.Size = new System.Drawing.Size(118, 29);
+            this.lblRamTotal.TabIndex = 1;
+            this.lblRamTotal.Text = "RamTotal";
+            // 
+            // lblRamFree
+            // 
+            this.lblRamFree.AutoSize = true;
+            this.lblRamFree.Font = new System.Drawing.Font("Hanuman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRamFree.Location = new System.Drawing.Point(413, 47);
+            this.lblRamFree.Name = "lblRamFree";
+            this.lblRamFree.Size = new System.Drawing.Size(78, 29);
+            this.lblRamFree.TabIndex = 2;
+            this.lblRamFree.Text = "label2";
+            // 
+            // lblRamPercent
+            // 
+            this.lblRamPercent.AutoSize = true;
+            this.lblRamPercent.Font = new System.Drawing.Font("Hanuman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRamPercent.Location = new System.Drawing.Point(413, 88);
+            this.lblRamPercent.Name = "lblRamPercent";
+            this.lblRamPercent.Size = new System.Drawing.Size(78, 29);
+            this.lblRamPercent.TabIndex = 3;
+            this.lblRamPercent.Text = "label3";
+            // 
+            // lblCpuPercent
+            // 
+            this.lblCpuPercent.AutoSize = true;
+            this.lblCpuPercent.Font = new System.Drawing.Font("Hanuman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCpuPercent.Location = new System.Drawing.Point(413, 128);
+            this.lblCpuPercent.Name = "lblCpuPercent";
+            this.lblCpuPercent.Size = new System.Drawing.Size(77, 29);
+            this.lblCpuPercent.TabIndex = 4;
+            this.lblCpuPercent.Text = "label4";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Hanuman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(252, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 29);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "CPU %";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Hanuman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(252, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 29);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "RAM %";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Hanuman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(252, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 29);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "RAM Free";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Hanuman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(252, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 29);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "RAMTotal";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsForms.Properties.Resources.giphy;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(235, 231);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(535, 231);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblCpuPercent);
+            this.Controls.Add(this.lblRamPercent);
+            this.Controls.Add(this.lblRamFree);
+            this.Controls.Add(this.lblRamTotal);
             this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblRamTotal;
+        private System.Windows.Forms.Label lblRamFree;
+        private System.Windows.Forms.Label lblRamPercent;
+        private System.Windows.Forms.Label lblCpuPercent;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- WindowsForms/Form1.cs
+++ WindowsForms/Form1.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Dynamic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Management;
 using System.Net;
+using System.Net.Mail;
 using System.Net.Sockets;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace WindowsForms
@@ -17,40 +23,244 @@ namespace WindowsForms
         public Form1()
         {
             InitializeComponent();
-            // BaseForm.SetForm(this);
+            timer1.Start();
+        }
+
+
+
+        private string weekday(int d, int m, int y)
+        {
+            string dayName = string.Empty;
+            int LeapYears = (int)y / 4;
+            long a = (y - LeapYears) * 365 + LeapYears * 366;
+            if (m >= 2) a += 31;
+            if (m >= 3 && (int)y / 4 == y / 4) a += 29;
+            else if (m >= 3) a += 28;
+            if (m >= 4) a += 31;
+            if (m >= 5) a += 30;
+            if (m >= 6) a += 31;
+            if (m >= 7) a += 30;
+            if (m >= 8) a += 31;
+            if (m >= 9) a += 31;
+            if (m >= 10) a += 30;
+            if (m >= 11) a += 31;
+            if (m == 12) a += 30;
+            a += d;
+            int b = (int)(a - 2) % 7;
+            switch (b)
+            {
+                case 1:
+                    dayName = "Monday";
+                    break;
+
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+
+                case 4:
+                    dayName = "Thursday";
+                    break;
+
+                case 5:
+                    dayName = "Friday";
+                    break;
+
+                case 6:
+                    dayName = "Saturday";
+                    break;
+
+                case 7:
+                    dayName = "Sunday";
+                    break;
+            }
+            return dayName;
+        }
+
+        private object getCPU()
+        {
+            var process = Process.GetCurrentProcess();
+
+            // Preparing variable for application instance name
+            var name = string.Empty;
+
+            foreach (var instance in new PerformanceCounterCategory("Process").GetInstanceNames())
+            {
+                if (instance.StartsWith(process.ProcessName))
+                {
+                    using (var processId = new PerformanceCounter("Process", "ID Process", instance, true))
+                    {
+                        if (process.Id == (int)processId.RawValue)
+                        {
+                            name = instance;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            var cpu = new PerformanceCounter("Process", "% Processor Time", name, true);
+            var ram = new PerformanceCounter("Process", "Private Bytes", name, true);
+
+            // Getting first initial values
+            cpu.NextValue();
+            ram.NextValue();
+
+            // Creating delay to get correct values of CPU usage during next query
+            Thread.Sleep(500);
+
+            dynamic result = new ExpandoObject();
+
+            // If system has multiple cores, that should be taken into account
+            result.CPU = Math.Round(cpu.NextValue() / Environment.ProcessorCount, 2);
+            // Returns number of MB consumed by application
+            result.RAM = Math.Round(ram.NextValue() / 1024 / 1024, 2);
+
+            return result;
+        }
+
+        private PerformanceCounter cpuCounter;
+        private PerformanceCounter ramCounter;
+
+        private string getCurrentCpuUsage()
+        {
+            cpuCounter = new PerformanceCounter();
+
+            cpuCounter.CategoryName = "Processor";
+            cpuCounter.CounterName = "% Processor Time";
+            cpuCounter.InstanceName = "_Total";
+
+            ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+
+            return cpuCounter.NextValue() + "%";
+        }
+
+        private String GetTotalRAM()
+        {
+            ManagementClass mc = new ManagementClass("Win32_ComputerSystem");
+            ManagementObjectCollection moc = mc.GetInstances();
+            foreach (ManagementObject item in moc)
+            {
+                return Convert.ToString(Math.Round(Math.Round(Convert.ToDouble(item.Properties["TotalPhysicalMemory"].Value) / 1048576, 0) / 1024, 2)) + " GB";
+                //return Convert.ToString(Math.Round(Convert.ToDouble(item.Properties["TotalPhysicalMemory"].Value) / 1048576, 0) / 1024) + " MB";
+            }
+
+            return "RAMsize";
+        }
+
+        private string GetFreeRAM()
+        {
+            cpuCounter = new PerformanceCounter();
+
+            cpuCounter.CategoryName = "Processor";
+            cpuCounter.CounterName = "% Processor Time";
+            cpuCounter.InstanceName = "_Total";
+            ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+            return (Math.Round(ramCounter.NextValue() / 1024, 2)) + " GB";
+        }
+
+        private string GetPercentRAM()
+        {
+            string percent = string.Empty;
+            double total = double.Parse(GetTotalRAM().Replace("GB", "").Trim());
+            double use = double.Parse(GetFreeRAM().Replace("GB", "").Trim());
+            double per = 100 - (use * 100) / total;
+            int result = Convert.ToInt32(per);
+            percent = result.ToString() + "%";
+            return percent;
+        }
+
+        private string FindPercentRAM()
+        {
+            var wmiObject = new ManagementObjectSearcher("select * from Win32_OperatingSystem");
+
+            var memoryValues = wmiObject.Get().Cast<ManagementObject>().Select(mo => new
+            {
+                FreePhysicalMemory = Double.Parse(mo["FreePhysicalMemory"].ToString()),
+                TotalVisibleMemorySize = Double.Parse(mo["TotalVisibleMemorySize"].ToString())
+            }).FirstOrDefault();
+            string percent = string.Empty;
+            if (memoryValues != null)
+            {
+                int result = Convert.ToInt32(((memoryValues.TotalVisibleMemorySize - memoryValues.FreePhysicalMemory) / memoryValues.TotalVisibleMemorySize) * 100);
+                percent = result + "%";
+            }
+            return percent;
+        }
+
+        private string GetCPUPercent()
+        {
+            string result = string.Empty;
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("select * from Win32_PerfFormattedData_PerfOS_Processor");
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                var usage = obj["PercentProcessorTime"];
+                var name = obj["Name"];
+                result = usage.ToString();
+                // Console.WriteLine(name + " : " + usage);
+            }
+            return result + "%";
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            lblRamPercent.Text = FindPercentRAM();
+            lblRamFree.Text = GetFreeRAM();
+            lblCpuPercent.Text = GetCPUPercent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Type myType = typeof(Person);
+            lblRamTotal.Text = GetTotalRAM();
+            lblRamPercent.Text = FindPercentRAM();
+            lblRamFree.Text = GetFreeRAM();
+            lblCpuPercent.Text = GetCPUPercent();
+            // string Query = "SELECT Capacity FROM Win32_PhysicalMemory";
+            // ManagementObjectSearcher searcher = new ManagementObjectSearcher(Query);
 
-            // Get the fields of the specified class.
-            FieldInfo[] myField = myType.GetFields(BindingFlags.Public | BindingFlags.Instance);
-            MemberInfo[] mem = myType.GetDefaultMembers();
-            //myType.GetDefaultMembers
-
-            Console.WriteLine("\nDisplaying fields that have SpecialName attributes:\n");
-            for (int i = 0; i < myField.Length; i++)
+            //MessageBox.Show("Ram Total: " + GetTotalRAM() + "\nRam Using: " + GetFreeRAM() + "\nRAM using (%): " + GetPercentRAM() + "\nRAM using (%): " + FindPercentRAM()
+            //    + "\nCPU(%) " + GetCPUPercent());
+            //MessageBox.Show("Using C# for Date 25 June 2025 is : " + new DateTime(2025, 6, 25).DayOfWeek);
+            var client = new SmtpClient("smtp.gmail.com", 587)
             {
-                // Determine whether or not each field is a special name.
-                if (myField[i].IsSpecialName)
-                {
-                    //Console.WriteLine("The field {0} has a SpecialName attribute.", myField[i].Name);
-                    MessageBox.Show(myField[i].Name);
-                }
-            }
+                Credentials = new NetworkCredential("videolesson2907@gmail.com", "Davin01"),
+                EnableSsl = true
+            };
+            //client.Send("videolesson2907@gmail.com", "pheaklesson@gmail.com", "test", "Test from c#");
+            //MessageBox.Show("Sent");
 
-            TypeInfo t = typeof(Person).GetTypeInfo();
-            IEnumerable<PropertyInfo> pList = t.DeclaredProperties;
-            IEnumerable<MethodInfo> mList = t.DeclaredMethods;
-            List<string> getFiels = new List<string>();
-            foreach (PropertyInfo p in pList)
-            {
-                getFiels.Add(p.Name);
-            }
-            Person per = new Person();
+            /*  Type myType = typeof(Person);
 
-            MessageBox.Show(getFiels + "");
+              // Get the fields of the specified class.
+              FieldInfo[] myField = myType.GetFields(BindingFlags.Public | BindingFlags.Instance);
+              MemberInfo[] mem = myType.GetDefaultMembers();
+              //myType.GetDefaultMembers
+
+              Console.WriteLine("\nDisplaying fields that have SpecialName attributes:\n");
+              for (int i = 0; i < myField.Length; i++)
+              {
+                  // Determine whether or not each field is a special name.
+                  if (myField[i].IsSpecialName)
+                  {
+                      //Console.WriteLine("The field {0} has a SpecialName attribute.", myField[i].Name);
+                      MessageBox.Show(myField[i].Name);
+                  }
+              }
+
+              TypeInfo t = typeof(Person).GetTypeInfo();
+              IEnumerable<PropertyInfo> pList = t.DeclaredProperties;
+              IEnumerable<MethodInfo> mList = t.DeclaredMethods;
+              List<string> getFiels = new List<string>();
+              foreach (PropertyInfo p in pList)
+              {
+                  getFiels.Add(p.Name);
+              }
+              Person per = new Person();
+
+              MessageBox.Show(getFiels + "");*/
 
             //string data = "";
             //StreamReader reader = new StreamReader(@"C:\WINDOWS\system32");
@@ -208,11 +418,11 @@ namespace WindowsForms
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            textBox1.BorderStyle = BorderStyle.None;
-            Pen p = new Pen(Color.CornflowerBlue);
-            Graphics g = e.Graphics;
-            int variance = 3;
-            g.DrawRectangle(p, new Rectangle(textBox1.Location.X - variance, textBox1.Location.Y - variance, textBox1.Width + variance, textBox1.Height + variance));
+            // textBox1.BorderStyle = BorderStyle.None;
+            // Pen p = new Pen(Color.CornflowerBlue);
+            //Graphics g = e.Graphics;
+            //int variance = 3;
+            // g.DrawRectangle(p, new Rectangle(textBox1.Location.X - variance, textBox1.Location.Y - variance, textBox1.Width + variance, textBox1.Height + variance));
         }
     }
 }
